7d4f0397de61ff724a89e6571bf8b5802a987072
kdeane <katrina.deane@lin-magdeburg.de>
2020-11-16 16:40:25 +0100

    fixed up loops to call in TA or ST dataset one at a time and save all results in one output summary

--- RAnova.R
+++ RAnova.R
@@ -4,54 +4,76 @@ home = getwd()
 datapath = file.path(home,"Data")
 
 sink(file=file.path(datapath,"AvrecPeakStats","ANOVASummary.txt"), type=c("output")) #save all output to txt file!
-stimtype = list("CL","AM")
+TAorST   = list("TA", "ST")
+stimtype = list("CL", "AM")
+layer    = list("All", "I_II", "IV", "V", "VI")
 comptype = list("KIT vs KIC", "KIT vs KIV", "KIC vs KIV") #,"KIT vs KIV", "KIC vs KIV"
 
-for (iStim in 1:length(stimtype)) {
-  print(paste0("=========================== ANOVAs FOR STIM TYPE ", stimtype[iStim], " ==========================="))
-  # load full dataset in of appropriate stim type
-  peakdata = read.csv(file = file.path(datapath,paste0("AVRECPeak",stimtype[1],".csv")))
-  # pull out stim frequencies present to loop through
-  stimfreq = unique(peakdata[c("ClickFreq")])
+for (iRun in 1:length(TAorST)) {
   
-  for (iFreq in 1:nrow(stimfreq)) {
+  if (TAorST[iRun] == "TA") {
+    print(paste0("============================== Trial Average =============================="))
+  } else {
+    print(paste0("============================== Single Trial  =============================="))
+  } # one more if statement when loading the dataset in in the next step
     
-    print(paste0("===================== STIMULUS FREQUENCY ", stimfreq[iFreq,1], " Hz ====================="))
+  for (iStim in 1:length(stimtype)) {
     
-    # cut down to one stim frequency 
-    FDat = subset(peakdata, ClickFreq == stimfreq[iFreq,1])
+    print(paste0("=========================== ANOVAs FOR STIM TYPE ", stimtype[iStim], " ==========================="))
     
-    # loop through each response per stimuli 
-    for (iOrd in 1:stimfreq[iFreq,1]) {
+    # load full dataset in of appropriate stim type
+    if (TAorST[iRun] == "TA") {
+      peakdata = read.csv(file = file.path(datapath,paste0("AVRECPeak",stimtype[1],".csv")))
+    } else {
+      peakdata = read.csv(file = file.path(datapath,paste0("AVRECPeak",stimtype[1],"ST.csv")))
+    }
+    # pull out stim frequencies present to loop through
+    stimfreq = unique(peakdata[c("ClickFreq")])
+    
+    for (iLay in 1:length(layer)) {
       
-      print(paste0("=============== RESPONSE ", iOrd, " OF ", stimfreq[iFreq,1], " ==============="))
-      OFDat = subset(FDat, OrderofClick == iOrd)
+      print(paste0("=========================== LAYER ", layer[iLay], " ==========================="))
+      LDay = subset(peakdata, Layer == layer[iLay])
       
-      for (iCmp in 1:length(comptype)) {
+      for (iFreq in 1:nrow(stimfreq)) {
         
-        if (comptype[iCmp] == "KIT vs KIC") {
-          subDat = subset(OFDat, !(Group == "KIV"))
-        } else if (comptype[iCmp] == "KIT vs KIV") {
-          subDat = subset(OFDat, !(Group == "KIC"))
-        } else if (comptype[iCmp] == "KIC vs KIV") {
-          subDat = subset(OFDat, !(Group == "KIT"))
-        }
+        print(paste0("===================== STIMULUS FREQUENCY ", stimfreq[iFreq,1], " Hz ====================="))
         
-        print(paste0("========= PEAK AMPLITUDE ", comptype[iCmp], " =========")) 
-        res.aov3 = aov(PeakAmp ~ Group * Measurement, data = subDat)
-        print(summary(res.aov3))
+        # cut down to one stim frequency 
+        FDat = subset(LDay, ClickFreq == stimfreq[iFreq,1])
         
-        print(paste0("========= PEAK LATITUDE ", comptype[iCmp], " =========")) 
-        res.aov3 = aov(PeakLat ~ Group * Measurement, data = subDat)
-        print(summary(res.aov3))
-        
-        print(paste0("========= ROOT MEAN SQUARE ", comptype[iCmp], " =========")) 
-        res.aov3 = aov(RMS ~ Group * Measurement, data = subDat)
-        print(summary(res.aov3))
-      
-      }
-    } # order of click
-  } # which stim frequency
-} # which stim type, CL or AM
-
+        # loop through each response per stimuli 
+        for (iOrd in 1:stimfreq[iFreq,1]) {
+          
+          print(paste0("=============== RESPONSE ", iOrd, " OF ", stimfreq[iFreq,1], " ==============="))
+          OFDat = subset(FDat, OrderofClick == iOrd)
+          
+          for (iCmp in 1:length(comptype)) {
+            
+            if (comptype[iCmp] == "KIT vs KIC") {
+              subDat = subset(OFDat, !(Group == "KIV"))
+            } else if (comptype[iCmp] == "KIT vs KIV") {
+              subDat = subset(OFDat, !(Group == "KIC"))
+            } else if (comptype[iCmp] == "KIC vs KIV") {
+              subDat = subset(OFDat, !(Group == "KIT"))
+            }
+            
+            print(paste0("========= PEAK AMPLITUDE ", comptype[iCmp], " =========")) 
+            res.aov3 = aov(PeakAmp ~ Group * Measurement, data = subDat)
+            print(summary(res.aov3))
+            
+            print(paste0("========= PEAK LATITUDE ", comptype[iCmp], " =========")) 
+            res.aov3 = aov(PeakLat ~ Group * Measurement, data = subDat)
+            print(summary(res.aov3))
+            
+            print(paste0("========= ROOT MEAN SQUARE ", comptype[iCmp], " =========")) 
+            res.aov3 = aov(RMS ~ Group * Measurement, data = subDat)
+            print(summary(res.aov3))
+          
+          } # which comparison
+        } # order of click
+      } # which stim frequency
+    } # which layer
+  } # which stim type, CL or AM
+} # Trial average or single trial
 sink() #restore output to console and finish using txt file
