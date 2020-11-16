2da677bef5954a7b85352bfa8a180fc2812626bd
kdeane <katrina.deane@lin-magdeburg.de>
2020-11-16 13:03:07 +0100

    printed summary result to include in txt saveout, corrected indexing syntax, corrected dataframe subsetting (different variable names)

--- RAnova.R
+++ RAnova.R
@@ -5,7 +5,7 @@ datapath = file.path(home,"Data")
 
 sink(file=file.path(datapath,"AvrecPeakStats","ANOVASummary.txt"), type=c("output")) #save all output to txt file!
 stimtype = list("CL","AM")
-comptype = list("KIT vs KIC","KIT vs KIV","KIC vs KIV")
+comptype = list("KIT vs KIC", "KIT vs KIV", "KIC vs KIV") #,"KIT vs KIV", "KIC vs KIV"
 
 for (iStim in 1:length(stimtype)) {
   print(paste0("=========================== ANOVAs FOR STIM TYPE ", stimtype[iStim], " ==========================="))
@@ -16,38 +16,38 @@ for (iStim in 1:length(stimtype)) {
   
   for (iFreq in 1:nrow(stimfreq)) {
     
-    print(paste0("===================== STIMULUS FREQUENCY ", stimfreq[1,iFreq], " Hz ====================="))
+    print(paste0("===================== STIMULUS FREQUENCY ", stimfreq[iFreq,1], " Hz ====================="))
     
     # cut down to one stim frequency 
-    Dat = subset(peakdata, ClickFreq == stimfreq[1,iFreq])
+    FDat = subset(peakdata, ClickFreq == stimfreq[iFreq,1])
     
     # loop through each response per stimuli 
-    for (iOrd in 1:stimfreq[1,iFreq]) {
+    for (iOrd in 1:stimfreq[iFreq,1]) {
       
-      print(paste0("=============== RESPONSE ", iOrd, " OF ", ordernum, " ==============="))
-      Dat = subset(Dat, OrderofClick == iOrd)
+      print(paste0("=============== RESPONSE ", iOrd, " OF ", stimfreq[iFreq,1], " ==============="))
+      OFDat = subset(FDat, OrderofClick == iOrd)
       
       for (iCmp in 1:length(comptype)) {
         
-        if (comptype[comptype[iCmp]] == "KIT vs KIC") {
-          subDat = subset(Dat, !(Group == "KIV"))
-        } else if (comptype[comptype[iCmp]] == "KIT vs KIV") {
-          subDat = subset(Dat, !(Group == "KIC"))
-        } else if (comptype[comptype[iCmp]] == "KIC vs KIV") {
-          subDat = subset(Dat, !(Group == "KIT"))
+        if (comptype[iCmp] == "KIT vs KIC") {
+          subDat = subset(OFDat, !(Group == "KIV"))
+        } else if (comptype[iCmp] == "KIT vs KIV") {
+          subDat = subset(OFDat, !(Group == "KIC"))
+        } else if (comptype[iCmp] == "KIC vs KIV") {
+          subDat = subset(OFDat, !(Group == "KIT"))
         }
         
         print(paste0("========= PEAK AMPLITUDE ", comptype[iCmp], " =========")) 
         res.aov3 = aov(PeakAmp ~ Group * Measurement, data = subDat)
-        summary(res.aov3)
+        print(summary(res.aov3))
         
         print(paste0("========= PEAK LATITUDE ", comptype[iCmp], " =========")) 
         res.aov3 = aov(PeakLat ~ Group * Measurement, data = subDat)
-        summary(res.aov3)
+        print(summary(res.aov3))
         
         print(paste0("========= ROOT MEAN SQUARE ", comptype[iCmp], " =========")) 
         res.aov3 = aov(RMS ~ Group * Measurement, data = subDat)
-        summary(res.aov3)
+        print(summary(res.aov3))
       
       }
     } # order of click
