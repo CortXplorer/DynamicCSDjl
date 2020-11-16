a4917b73e24231a870e4b53dce0db7d4a674ad15
kdeane <katrina.deane@lin-magdeburg.de>
2020-11-16 09:39:00 +0100

    Added one method to save singular output to text file

--- RAnova.R
+++ RAnova.R
@@ -1,10 +1,20 @@
+## Set working directory and create necessary file paths
 setwd("D:/DynamicCSDjl")
+home = getwd()
+datapath = file.path(home,"Data")
 
 # load full dataset in
-peakdata = read.csv(file = 'AVRECPeakAM.csv')
+peakdata = read.csv(file = file.path(datapath,"AVRECPeakAM.csv"))
 
 # cut down to one stim freq on the first stim response
 Dat = subset(peakdata, ClickFreq == 2 & OrderofClick ==1)
 
 res.aov3 <- aov(PeakAmp ~ Group * Measurement, data = Dat)
-summary(res.aov3)
+DatSum = summary(res.aov3)
+
+#method one to save out one at a time
+capture.output(DatSum,file="Datsum.txt",append = FALSE,type = c("output","message"),split=FALSE)
+
+print("Hello there boss")
+
+
