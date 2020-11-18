a8e9e5fa3ca71ded36a807fa829ccd44343b45c6
kdeane <katrina.deane@lin-magdeburg.de>
2020-11-18 09:36:25 +0100

    between fix: continue from loop if not enough data rows (<=3), within fix: CohenD if statement to output NaN if pvalue is NaN

--- functions/AvrecPeakStats.jl
+++ functions/AvrecPeakStats.jl
@@ -1,12 +1,12 @@
-function PeakRatio_Between(data,StatTab,whichstim="2Hz",stimtype="CL",trialtype="TA")
+function PeakRatio_Between(data,Stat,whichstim="2Hz",stimtype="CL",trialtype="TA")
     # Input: folder path data, table for ratio of 2 hz and 5 hz, all groups being tested
     # Output: table in folder Data/AvrecPeakStats which contains the pvalue result for an unequal test of variance (2 sample t test) of the ratio of final to first peak response between each group
 
     # Setup for simple 2 sample t test from HypothesisTests ->
     # 3 between group comparisons across 5 measurement types and 5 layers:
     CompList    = ["KIC vs KIT" "KIC vs KIV" "KIT vs KIV"]
-    LayList     = unique(StatTab[:,:Layer])
-    MeasList    = unique(StatTab[:,:Measurement])
+    LayList     = unique(Stat[:,:Layer])
+    MeasList    = unique(Stat[:,:Measurement])
     # containers for the table columns: 
     Comparison  = String[]
     Layer       = String[]
@@ -18,8 +18,8 @@ function PeakRatio_Between(data,StatTab,whichstim="2Hz",stimtype="CL",trialtype=
     # loop through the above - create a table output! :) 
     for iComp = 1:length(CompList)
         # pull out groups from ratio table
-        G1 = StatTab[StatTab[:,:Group] .== CompList[iComp][1:3] ,:]
-        G2 = StatTab[StatTab[:,:Group] .== CompList[iComp][8:10],:]
+        G1 = Stat[Stat[:,:Group] .== CompList[iComp][1:3] ,:]
+        G2 = Stat[Stat[:,:Group] .== CompList[iComp][8:10],:]
 
         for iMeas = 1:length(MeasList)
             # pull out the measurment for those groups
@@ -68,15 +68,15 @@ function PeakRatio_Between(data,StatTab,whichstim="2Hz",stimtype="CL",trialtype=
     CSV.write(name, BetweenGroup)
 end
 
-function PeakRatio_Within(data,StatTab,whichstim="2Hz",stimtype="CL",trialtype="TA")
+function PeakRatio_Within(data,Stat,whichstim="2Hz",stimtype="CL",trialtype="TA")
     # Input: folder path data, table for ratio of 2 hz and 5 hz, all groups being tested
     # Output: table in folder Data/AvrecPeakStats which contains the pvalue result for an equal test of variance (2 sample t test) of the ratio of final to first peak response between each measurement to the first
 
     # Setup for simple 2 sample t test from HypothesisTests ->
     # 4 within group comparisons per group across 5 measurement types and 5 layers:
     GroupList    = ["KIC" "KIT"] #  "KIV"
-    LayList     = unique(StatTab[:,:Layer])
-    MeasList    = unique(StatTab[:,:Measurement])
+    LayList     = unique(Stat[:,:Layer])
+    MeasList    = unique(Stat[:,:Measurement])
     # containers for the table columns: 
     Group       = String[]
     Layer       = String[]
@@ -92,7 +92,7 @@ function PeakRatio_Within(data,StatTab,whichstim="2Hz",stimtype="CL",trialtype="
     # loop through the above - create a table output! :) 
     for iGrp = 1:length(GroupList)
         # pull out groups from ratio table
-        Gr_Stat = StatTab[StatTab[:,:Group] .== GroupList[iGrp],:]
+        Gr_Stat = Stat[Stat[:,:Group] .== GroupList[iGrp],:]
 
         for iLay = 1:length(LayList)
             # pull out the layer one at a time
@@ -151,15 +151,15 @@ function PeakRatio_Within(data,StatTab,whichstim="2Hz",stimtype="CL",trialtype="
 end
 
 
-function Peak1_Between(data,StatTab,whichpeak="First",whichstim="2Hz",stimtype="CL",trialtype="TA")
+function Peak1_Between(data,Stat,whichpeak="1st",whichstim="2Hz",stimtype="CL",trialtype="TA")
     # Input: folder path data, table for peak amp/lat of 2 hz or 5 hz, all groups being tested
     # Output: table in folder Data/AvrecPeakStats which contains the pvalue result for an unequal test of variance (2 sample t test) of the peak amp and lat of the first response between each group
 
     # Setup for simple 2 sample t test from HypothesisTests ->
     # 3 between group comparisons across 5 measurement types and 5 layers:
     CompList    = ["KIC vs KIT" "KIC vs KIV" "KIT vs KIV"]
-    LayList     = unique(StatTab[:,:Layer])
-    MeasList    = unique(StatTab[:,:Measurement])
+    LayList     = unique(Stat[:,:Layer])
+    MeasList    = unique(Stat[:,:Measurement])
     # containers for the table columns: 
     Comparison  = String[]
     Layer       = String[]
@@ -172,8 +172,8 @@ function Peak1_Between(data,StatTab,whichpeak="First",whichstim="2Hz",stimtype="
     # loop through the above - create a table output! :) 
     for iComp = 1:length(CompList)
         # pull out groups from ratio table
-        G1 = StatTab[StatTab[:,:Group] .== CompList[iComp][1:3] ,:]
-        G2 = StatTab[StatTab[:,:Group] .== CompList[iComp][8:10],:]
+        G1 = Stat[Stat[:,:Group] .== CompList[iComp][1:3] ,:]
+        G2 = Stat[Stat[:,:Group] .== CompList[iComp][8:10],:]
 
         for iMeas = 1:length(MeasList)
             # pull out the measurment for those groups
@@ -192,9 +192,10 @@ function Peak1_Between(data,StatTab,whichpeak="First",whichstim="2Hz",stimtype="
                 G2_layrms = filter(row -> ! isnan(row.RMS), G2_lay)
 
                 # find the p value outcome for this comparison at both stimuli conditions
-                if isempty(G1_lay) || isempty(G2_lay)
+                if isempty(G1_lay) || isempty(G2_lay) || size(G1_layamp)[1] <= 3 || size(G2_layamp)[1] <= 3
                     continue 
                 end
+
                 PAmp[count[1]]  = pvalue(UnequalVarianceTTest(G1_layamp[:,:PeakAmp],G2_layamp[:,:PeakAmp]))
                 CDAmp[count[1]] = effectsize(CohenD(G1_layamp[:,:PeakAmp],G2_layamp[:,:PeakAmp]))
                 PLat[count[1]]  = pvalue(UnequalVarianceTTest(G1_laylat[:,:PeakLat],G2_laylat[:,:PeakLat]))
@@ -224,33 +225,33 @@ function Peak1_Between(data,StatTab,whichpeak="First",whichstim="2Hz",stimtype="
     CSV.write(name, BetweenGroup)
 end
 
-function Peak1_Within(data,StatTab,whichpeak="First",whichstim="2Hz",stimtype="CL",trialtype="TA")
+function Peak1_Within(data,Stat,whichpeak="1st",whichstim="2Hz",stimtype="CL",trialtype="TA")
     # Input: folder path data, table for peak amp/lat of 2 hz and 5 hz, all groups being tested
     # Output: table in folder Data/AvrecPeakStats which contains the pvalue result for an equal test of variance (2 sample t test) of the peak amp and lat of the first response between each measurement to the first
 
     # Setup for simple 2 sample t test from HypothesisTests ->
     # 4 within group comparisons per group across 5 measurement types and 5 layers:
     GroupList    = ["KIC" "KIT"] #"KIV"
-    LayList     = unique(StatTab[:,:Layer])
-    MeasList    = unique(StatTab[:,:Measurement])
+    LayList     = unique(Stat[:,:Layer])
+    MeasList    = unique(Stat[:,:Measurement])
     # containers for the table columns: 
     Group       = String[]
     Layer       = String[]
     # Measurement = String[]   
-    Prev1_Amp  = ones(length(GroupList) * length(LayList)) # number of rows 
-    Prev2_Amp,Prev3_Amp,Prev4_Amp  = ones(length(Prev1_Amp)),ones(length(Prev1_Amp)),ones(length(Prev1_Amp))
-    Prev1_Lat,Prev2_Lat,Prev3_Lat,Prev4_Lat  = ones(length(Prev1_Amp)),ones(length(Prev1_Amp)),ones(length(Prev1_Amp)),ones(length(Prev1_Amp))
-    Prev1_RMS,Prev2_RMS,Prev3_RMS,Prev4_RMS  = ones(length(Prev1_Amp)),ones(length(Prev1_Amp)),ones(length(Prev1_Amp)),ones(length(Prev1_Amp))
+    Prev1_Amp  = ones(length(GroupList) * length(LayList))*-1 # number of rows 
+    Prev2_Amp,Prev3_Amp,Prev4_Amp  = ones(length(Prev1_Amp))*-1,ones(length(Prev1_Amp))*-1,ones(length(Prev1_Amp))*-1
+    Prev1_Lat,Prev2_Lat,Prev3_Lat,Prev4_Lat  = ones(length(Prev1_Amp))*-1,ones(length(Prev1_Amp))*-1,ones(length(Prev1_Amp))*-1,ones(length(Prev1_Amp))*-1
+    Prev1_RMS,Prev2_RMS,Prev3_RMS,Prev4_RMS  = ones(length(Prev1_Amp))*-1,ones(length(Prev1_Amp))*-1,ones(length(Prev1_Amp))*-1,ones(length(Prev1_Amp))*-1
     
-    Prev1_CDAmp,Prev2_CDAmp,Prev3_CDAmp,Prev4_CDAmp  = ones(length(Prev1_Amp)),ones(length(Prev1_Amp)),ones(length(Prev1_Amp)),ones(length(Prev1_Amp))
-    Prev1_CDLat,Prev2_CDLat,Prev3_CDLat,Prev4_CDLat  = ones(length(Prev1_Amp)),ones(length(Prev1_Amp)),ones(length(Prev1_Amp)),ones(length(Prev1_Amp))
-    Prev1_CDRMS,Prev2_CDRMS,Prev3_CDRMS,Prev4_CDRMS  = ones(length(Prev1_Amp)),ones(length(Prev1_Amp)),ones(length(Prev1_Amp)),ones(length(Prev1_Amp))
+    Prev1_CDAmp,Prev2_CDAmp,Prev3_CDAmp,Prev4_CDAmp  = ones(length(Prev1_Amp))*-1,ones(length(Prev1_Amp))*-1,ones(length(Prev1_Amp))*-1,ones(length(Prev1_Amp))*-1
+    Prev1_CDLat,Prev2_CDLat,Prev3_CDLat,Prev4_CDLat  = ones(length(Prev1_Amp))*-1,ones(length(Prev1_Amp))*-1,ones(length(Prev1_Amp))*-1,ones(length(Prev1_Amp))*-1
+    Prev1_CDRMS,Prev2_CDRMS,Prev3_CDRMS,Prev4_CDRMS  = ones(length(Prev1_Amp))*-1,ones(length(Prev1_Amp))*-1,ones(length(Prev1_Amp))*-1,ones(length(Prev1_Amp))*-1
 
     count       = [1]
     # loop through the above - create a table output! :) 
     for iGrp = 1:length(GroupList)
         # pull out groups from ratio table
-        Gr_Stat = StatTab[StatTab[:,:Group] .== GroupList[iGrp],:]
+        Gr_Stat = Stat[Stat[:,:Group] .== GroupList[iGrp],:]
         
         for iLay = 1:length(LayList)
             # pull out the layer one at a time
@@ -273,6 +274,9 @@ function Peak1_Within(data,StatTab,whichpeak="First",whichstim="2Hz",stimtype="C
             G4_amp = filter(row -> ! isnan(row.PeakAmp), Gr_4)
             G4_rms = filter(row -> ! isnan(row.RMS), Gr_4)
 
+            # if size(GP_amp)[1] >= 3 || size(G1_amp)[1] >= 3 || size(GP_rms)[1] >= 3 || size(G1_rms)[1] >= 3 
+            #     continue
+            # end - this is causing it to not take the pvalues for ANY of it but I do need a solution for the Cohen's D output if there is an error there
             # find the p value outcome for group between measurements
             Prev1_Amp[count[1]]  = pvalue(EqualVarianceTTest(GP_amp[:,:PeakAmp], G1_amp[:,:PeakAmp]))
             Prev2_Amp[count[1]]  = pvalue(EqualVarianceTTest(GP_amp[:,:PeakAmp], G2_amp[:,:PeakAmp]))
@@ -289,20 +293,55 @@ function Peak1_Within(data,StatTab,whichpeak="First",whichstim="2Hz",stimtype="C
             Prev3_RMS[count[1]]  = pvalue(EqualVarianceTTest(GP_rms[:,:RMS], G3_rms[:,:RMS]))
             Prev4_RMS[count[1]]  = pvalue(EqualVarianceTTest(GP_rms[:,:RMS], G4_rms[:,:RMS]))
             #Cohen's D
-            Prev1_CDAmp[count[1]]  = effectsize(CohenD(GP_amp[:,:PeakAmp], G1_amp[:,:PeakAmp]))
-            Prev2_CDAmp[count[1]]  = effectsize(CohenD(GP_amp[:,:PeakAmp], G2_amp[:,:PeakAmp]))
-            Prev3_CDAmp[count[1]]  = effectsize(CohenD(GP_amp[:,:PeakAmp], G3_amp[:,:PeakAmp]))
-            Prev4_CDAmp[count[1]]  = effectsize(CohenD(GP_amp[:,:PeakAmp], G4_amp[:,:PeakAmp]))
+            if isnan(Prev1_Amp[count[1]]) # I hate this solution so much but CohenD throws an error instead of giving a NaN output like the pvalue above
+                Prev1_CDAmp[count[1]]  = NaN
+                Prev1_CDLat[count[1]]  = NaN
+            else
+                Prev1_CDAmp[count[1]]  = effectsize(CohenD(GP_amp[:,:PeakAmp], G1_amp[:,:PeakAmp]))
+                Prev1_CDLat[count[1]]  = effectsize(CohenD(GP_amp[:,:PeakLat], G1_amp[:,:PeakLat]))
+            end
+            if isnan(Prev2_Amp[count[1]])
+                Prev2_CDAmp[count[1]]  = NaN
+                Prev2_CDLat[count[1]]  = NaN
+            else
+                Prev2_CDAmp[count[1]]  = effectsize(CohenD(GP_amp[:,:PeakAmp], G2_amp[:,:PeakAmp]))
+                Prev2_CDLat[count[1]]  = effectsize(CohenD(GP_amp[:,:PeakLat], G2_amp[:,:PeakLat]))
+            end
+            if isnan(Prev3_Amp[count[1]])
+                Prev3_CDAmp[count[1]]  = NaN
+                Prev3_CDLat[count[1]]  = NaN
+            else
+                Prev3_CDAmp[count[1]]  = effectsize(CohenD(GP_amp[:,:PeakAmp], G3_amp[:,:PeakAmp]))
+                Prev3_CDLat[count[1]]  = effectsize(CohenD(GP_amp[:,:PeakLat], G3_amp[:,:PeakLat]))
+            end
+            if isnan(Prev4_Amp[count[1]])
+                Prev4_CDAmp[count[1]]  = NaN
+                Prev4_CDLat[count[1]]  = NaN 
+            else
+                Prev4_CDAmp[count[1]]  = effectsize(CohenD(GP_amp[:,:PeakAmp], G4_amp[:,:PeakAmp]))
+                Prev4_CDLat[count[1]]  = effectsize(CohenD(GP_amp[:,:PeakLat], G4_amp[:,:PeakLat]))
+            end
 
-            Prev1_CDLat[count[1]]  = effectsize(CohenD(GP_amp[:,:PeakLat], G1_amp[:,:PeakLat]))
-            Prev2_CDLat[count[1]]  = effectsize(CohenD(GP_amp[:,:PeakLat], G2_amp[:,:PeakLat]))
-            Prev3_CDLat[count[1]]  = effectsize(CohenD(GP_amp[:,:PeakLat], G3_amp[:,:PeakLat]))
-            Prev4_CDLat[count[1]]  = effectsize(CohenD(GP_amp[:,:PeakLat], G4_amp[:,:PeakLat]))
-
-            Prev1_CDRMS[count[1]]  = effectsize(CohenD(GP_rms[:,:RMS], G1_rms[:,:RMS]))
-            Prev2_CDRMS[count[1]]  = effectsize(CohenD(GP_rms[:,:RMS], G2_rms[:,:RMS]))
-            Prev3_CDRMS[count[1]]  = effectsize(CohenD(GP_rms[:,:RMS], G3_rms[:,:RMS]))
-            Prev4_CDRMS[count[1]]  = effectsize(CohenD(GP_rms[:,:RMS], G4_rms[:,:RMS]))
+            if isnan(Prev1_RMS[count[1]])
+                Prev1_CDRMS[count[1]]  = NaN
+            else
+                Prev1_CDRMS[count[1]]  = effectsize(CohenD(GP_rms[:,:RMS], G1_rms[:,:RMS]))
+            end
+            if isnan(Prev2_RMS[count[1]])
+                Prev2_CDRMS[count[1]]  = NaN
+            else
+                Prev2_CDRMS[count[1]]  = effectsize(CohenD(GP_rms[:,:RMS], G2_rms[:,:RMS]))
+            end
+            if isnan(Prev3_RMS[count[1]])
+                Prev3_CDRMS[count[1]]  = NaN
+            else
+                Prev3_CDRMS[count[1]]  = effectsize(CohenD(GP_rms[:,:RMS], G3_rms[:,:RMS]))
+            end
+            if isnan(Prev4_RMS[count[1]])
+                Prev4_CDRMS[count[1]]  = NaN
+            else
+                Prev4_CDRMS[count[1]]  = effectsize(CohenD(GP_rms[:,:RMS], G4_rms[:,:RMS]))
+            end
 
             count[1] = count[1] + 1
             # store the appropriate tags at the same positions in their respective lists
@@ -311,6 +350,14 @@ function Peak1_Within(data,StatTab,whichpeak="First",whichstim="2Hz",stimtype="C
 
         end # layer
     end # comparison of which groups
+
+    Prev1_Amp,Prev2_Amp,Prev3_Amp,Prev4_Amp  = Prev1_Amp[Prev1_Amp .!= -1], Prev2_Amp[Prev2_Amp .!= -1], Prev3_Amp[Prev3_Amp .!= -1], Prev4_Amp[Prev4_Amp .!= -1]
+    Prev1_Lat,Prev2_Lat,Prev3_Lat,Prev4_Lat  = Prev1_Lat[Prev1_Lat .!= -1], Prev2_Lat[Prev2_Lat .!= -1], Prev3_Lat[Prev3_Lat .!= -1], Prev4_Lat[Prev4_Lat .!= -1]
+    Prev1_RMS,Prev2_RMS,Prev3_RMS,Prev4_RMS  = Prev1_RMS[Prev1_RMS .!= -1], Prev2_RMS[Prev2_RMS .!= -1], Prev3_RMS[Prev3_RMS .!= -1], Prev4_RMS[Prev4_RMS .!= -1]
+
+    Prev1_CDAmp,Prev2_CDAmp,Prev3_CDAmp,Prev4_CDAmp  = Prev1_CDAmp[Prev1_CDAmp .!= -1], Prev2_CDAmp[Prev2_CDAmp .!= -1], Prev3_CDAmp[Prev3_CDAmp .!= -1], Prev4_CDAmp[Prev4_CDAmp .!= -1]
+    Prev1_CDLat,Prev2_CDLat,Prev3_CDLat,Prev4_CDLat  = Prev1_CDLat[Prev1_CDLat .!= -1], Prev2_CDLat[Prev2_CDLat .!= -1], Prev3_CDLat[Prev3_CDLat .!= -1], Prev4_CDLat[Prev4_CDLat .!= -1]
+    Prev1_CDRMS,Prev2_CDRMS,Prev3_CDRMS,Prev4_CDRMS  = Prev1_CDRMS[Prev1_CDRMS .!= -1], Prev2_CDRMS[Prev2_CDRMS .!= -1], Prev3_CDRMS[Prev3_CDRMS .!= -1], Prev4_CDRMS[Prev4_CDRMS .!= -1]
 
     BetweenGroup = DataFrame(Group=Group, Layer=Layer, Prev1_Amp=Prev1_Amp, Prev2_Amp=Prev2_Amp, Prev3_Amp=Prev3_Amp, Prev4_Amp=Prev4_Amp,  Prev1_Lat=Prev1_Lat, Prev2_Lat=Prev2_Lat, Prev3_Lat=Prev3_Lat, Prev4_Lat=Prev4_Lat, Prev1_RMS=Prev1_RMS, Prev2_RMS=Prev2_RMS, Prev3_RMS=Prev3_RMS, Prev4_RMS=Prev4_RMS, Prev1_CDAmp=Prev1_CDAmp, Prev2_CDAmp=Prev2_CDAmp, Prev3_CDAmp=Prev3_CDAmp, Prev4_CDAmp=Prev4_CDAmp,  Prev1_CDLat=Prev1_CDLat, Prev2_CDLat=Prev2_CDLat, Prev3_CDLat=Prev3_CDLat, Prev4_CDLat=Prev4_CDLat, Prev1_CDRMS=Prev1_CDRMS, Prev2_CDRMS=Prev2_CDRMS, Prev3_CDRMS=Prev3_CDRMS, Prev4_CDRMS=Prev4_CDRMS)
 
