ee275fc6ff4674ad4d730bc31acc41e4f2fb54c9
kdeane <katrina.deane@lin-magdeburg.de>
2020-11-19 14:53:07 +0100

    added code up to the point of extracting a single CSD by condition

--- SpectralPermPipeline.jl
+++ SpectralPermPipeline.jl
@@ -1,4 +1,15 @@
 # compute scalograms 
+using MAT
+using JLD
+
+home    = @__DIR__
+path    = joinpath(home,"Data")
+anDat   = matread(joinpath(path,"KIC02_Data.mat"))
+jldDat  = load(joinpath(path,"KIT02_Data.jld"))
+
+var = 3
+name = joinpath(path,"vari3.jld")
+save(name, "t", t, "arr", z)
 
 
 # generate spectral plots (osci bands sorted by group and then frequency)
