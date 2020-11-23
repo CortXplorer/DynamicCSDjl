af2c7e8634d41264c2c511940fdc2837e033bd01
kdeane <katrina.deane@lin-magdeburg.de>
2020-11-23 20:16:43 +0100

    included stime list to parameters, set loops for animal, condition, measurement, stim freq, layer and created Dict output

--- SpectralPermPipeline.jl
+++ SpectralPermPipeline.jl
@@ -10,8 +10,8 @@ figs    = joinpath(home,"figs")
 include(joinpath(group,"callGroup.jl"))
 include(joinpath(func,"CWTfunc.jl"))
 
-# Initial Parameters
-params = (sampleRate=1000, startTime=-200, timeLimits=[-200 1377], frequencyLimits=[8 100], timeBandWidth=40, layers=["I_II","IV","V","VI"], osciBands=["alpha","beta low","beta high","gamma low","gamma high"], bandRanges=[[1:8...],[9:15...],[16:23...],[24:35...],[36:40...]])
+# Parameters
+params = (sampleRate=1000, startTime=-200, timeLimits=[-200 1377], frequencyLimits=[8 100], timeBandWidth=40, stimList=["2Hz","5Hz","10Hz","20Hz","40Hz"], layers=["I_II","IV","V","VI"], osciBands=["alpha","beta low","beta high","gamma low","gamma high"], bandRanges=[[1:8...],[9:15...],[16:23...],[24:35...],[36:40...]])
 # real ranges captured by these data: alpha=(8:12), beta_low=(13:18), beta_high=(19:30), gamma_low=(31:60), gamma_high=(61:100)
 
 foldername = "Spectral"
@@ -20,55 +20,81 @@ if !isdir(joinpath(data,foldername))
 end
 
 GroupList = ["KIC"]
+CLList  = ["preCL" "CL"]
 Group     = GroupList[1]
 
-# function runCwtCSD(home, Group, params)
+# function runCwtCSD(figs,home, Group, params)
 animalList,_,LIIList,LIVList,LVList,LVIList,CondList = callGroup(Group); 
 anipar    = (;LIIList,LIVList,LVList,LVIList)
-stimList  = ["2Hz" "5Hz" "10Hz" "20Hz" "40Hz"]
 
-iAn = 1
+# Loop through animals in Group
+Animal = Dict()
+for iAn = 1:length(animalList)
 
-# loop through animals iAn
-curAn   = animalList[iAn]
-anDat   = matread(joinpath(data,(curAn * "_Data.mat")));
-anMeas  = anDat["Data"]["measurement"]
+    # loop through animals iAn
+    curAn   = animalList[iAn]
+    anDat   = matread(joinpath(data,(curAn * "_Data.mat")));
+    anMeas  = anDat["Data"]["measurement"]
 
-CLList  = ["preCL" "CL"]
-iCond = 1
-# loop through condition list iCL
-curCond = CLList[iCond]
-MeasList = CondList[CLList[iCond]][iAn]
+    # Loop through the condition list
+    Condition = Dict()
+    for iCond = 1:length(CLList)
+        # loop through condition list iCL
+        MeasList = CondList[CLList[iCond]][iAn]
 
-iMeas = 1
-# loop through measurements in the list iMeas
-curMeas = CondList[CLList[iCond]][iAn][iMeas]
-curCond = curCond * "_" * string(iMeas)
-runthis = [curAn*"_"*curMeas] # generate full measurement name
+        # Loop through measurements
+        Measurement = Dict()
+        for iMeas = 1:length(MeasList)
 
-thisind = findall(anDat["Data"]["measurement"] .== runthis)[1][2] #[1][2] for extracting cartesian index
-iSti = 1
-# loop through stim frequencies
-curCSD  = anDat["Data"]["SglTrl_CSD"][thisind][iSti];
-iLay = 2
-# loop through layers
-if iLay == 2 # change call for each layer
-curChan = anipar.LIVList[iAn]
-end
-curLay = params.layers[iLay]
+            curMeas = CondList[CLList[iCond]][iAn][iMeas]
+            curCond = CLList[iCond] * "_" * string(iMeas)
+            runthis = [curAn*"_"*curMeas] # generate full measurement name
 
-if length(curChan) > 3 # take center 3 if greater than 3
-    centerChan = Int(ceil(length(curChan)/2))
-    curChan = curChan[centerChan-1:centerChan+1]
-end
+            thisind = findall(anDat["Data"]["measurement"] .== runthis)[1][2] #[1][2] for extracting cartesian index
 
-# meanLayCSD = mean(curCSD[curChan,:,:],dims=3)
-# ROI = mean(meanLayCSD,dims=1)
-ROI = mean(curCSD[curChan,:,:],dims=1)
+            # Loop through stimulation frequencies
+            StimFreq = Dict()
+            for iSti = 1:length(params.stimList)
 
-Datpower, Datphsco = CWTanalysis(ROI,params,curAn,curCond,curLay)
+                curCSD  = anDat["Data"]["SglTrl_CSD"][thisind][iSti];
+                curStim = params.stimList[iSti]
 
+                # Loop through layers
+                Layers = Dict()
+                for iLay = 1:length(params.layers)
 
+                    curLay = params.layers[iLay]
+                    if curLay == "I_II"
+                        curChan = anipar.LIIList[iAn]
+                    elseif curLay == "IV"
+                        curChan = anipar.LIVList[iAn]
+                    elseif curLay == "V"
+                        curChan = anipar.LVList[iAn]
+                    elseif curLay == "VI"
+                        curChan = anipar.LVIList[iAn]
+                    end
+
+                    if length(curChan) > 3 # take center 3 if greater than 3
+                        centerChan = Int(ceil(length(curChan)/2))
+                        curChan = curChan[centerChan-1:centerChan+1]
+                    end
+
+                    # Here is the region of interest accross trials
+                    ROI = mean(curCSD[curChan,:,:],dims=1)
+
+                    Datpower, Datphsco = CWTanalysis(figs,ROI,params,curAn,curCond,curLay,curStim)
+                    Layers[curLay] = Datpower, Datphsco;
+                end # Layers
+                StimFreq[curStim] = Layers
+            end # Stim Frequencies
+            Measurement[curCond] = StimFreq
+        end # Condition/Measurement
+        Condition[CLList[iCond]] = Measurement
+    end # Condition List
+    Animal[curAn] = Condition
+end # Animal
+# pull out probably dictionary of all animal data for use in group plots below
+# return SPower, PhaseCo
 # end function
 
 # generate spectral plots (osci bands sorted by group and then frequency)
