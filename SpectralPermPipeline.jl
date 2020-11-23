12cd80e645787fda2a300b4af8689e8d3ab3efeb
kdeane <katrina.deane@lin-magdeburg.de>
2020-11-23 16:05:42 +0100

    moved analysis up to both power and phase coherence scalogram plot and data output for one measurement

--- SpectralPermPipeline.jl
+++ SpectralPermPipeline.jl
@@ -8,9 +8,11 @@ group   = joinpath(home,"groups")
 figs    = joinpath(home,"figs")
 
 include(joinpath(group,"callGroup.jl"))
+include(joinpath(func,"CWTfunc.jl"))
 
 # Initial Parameters
-params = (sampleRate=1000, startTime=-200, timeLimits=[-200 1377], frequencyLimits=[5 100], voicesPerOctave=8, timeBandWidth=40, layers=["I_II","IV","V","VI"])
+params = (sampleRate=1000, startTime=-200, timeLimits=[-200 1377], frequencyLimits=[8 100], timeBandWidth=40, layers=["I_II","IV","V","VI"], osciBands=["alpha","beta low","beta high","gamma low","gamma high"], bandRanges=[[1:8...],[9:15...],[16:23...],[24:35...],[36:40...]])
+# real ranges captured by these data: alpha=(8:12), beta_low=(13:18), beta_high=(19:30), gamma_low=(31:60), gamma_high=(61:100)
 
 foldername = "Spectral"
 if !isdir(joinpath(data,foldername))
@@ -29,7 +31,7 @@ iAn = 1
 
 # loop through animals iAn
 curAn   = animalList[iAn]
-anDat   = matread(joinpath(data,(curAn * "_Data.mat")))
+anDat   = matread(joinpath(data,(curAn * "_Data.mat")));
 anMeas  = anDat["Data"]["measurement"]
 
 CLList  = ["preCL" "CL"]
@@ -47,23 +49,24 @@ runthis = [curAn*"_"*curMeas] # generate full measurement name
 thisind = findall(anDat["Data"]["measurement"] .== runthis)[1][2] #[1][2] for extracting cartesian index
 iSti = 1
 # loop through stim frequencies
-curCSD  = anDat["Data"]["SglTrl_CSD"][thisind][iSti]
+curCSD  = anDat["Data"]["SglTrl_CSD"][thisind][iSti];
 iLay = 2
 # loop through layers
 if iLay == 2 # change call for each layer
 curChan = anipar.LIVList[iAn]
 end
+curLay = params.layers[iLay]
 
 if length(curChan) > 3 # take center 3 if greater than 3
     centerChan = Int(ceil(length(curChan)/2))
     curChan = curChan[centerChan-1:centerChan+1]
 end
 
-meanLayCSD = mean(curCSD[curChan,:,:],dims=3)
-ROI = mean(meanLayCSD,dims=1)
+# meanLayCSD = mean(curCSD[curChan,:,:],dims=3)
+# ROI = mean(meanLayCSD,dims=1)
+ROI = mean(curCSD[curChan,:,:],dims=1)
 
-
-
+Datpower, Datphsco = CWTanalysis(ROI,params,curAn,curCond,curLay)
 
 
 # end function
--- functions/CWTfunc.jl
+++ functions/CWTfunc.jl
@@ -1,70 +1,79 @@
 
 function CWTanalysis(ROI,params,curAn="KIC02",curCond="preCL_1",curLay="IV")
 
-time = -1:(1/params.sampleRate):1
-ROItime = [-200:size(ROI)[2]-200-1...]
+    time     = -1:(1/params.sampleRate):1
+    ROItime  = [-200:size(ROI)[2]-200-1...]
 
-min_freq = params.frequencyLimits[1]
-max_freq = params.frequencyLimits[2]
-num_frex = params.timeBandWidth
+    min_freq = params.frequencyLimits[1]
+    max_freq = params.frequencyLimits[2]
+    num_frex = params.timeBandWidth
 
-n_data          = size(ROI)[2]*size(ROI)[3] #data points sampled * trials
-# NOTE: for multiple trial data, this will have to be examined, the same_conv function below required that I cut ROI to with [:] for a reasonably sized output to variable datconv
-n_wavelet       = length(time)
-n_convolution   = n_wavelet+n_data-1
-n_conv_pow2     = nextpow(2,n_convolution)
-half_wavelet    = Int((n_wavelet-1)/2)
+    n_data   = size(ROI)[2]*size(ROI)[3] #data points sampled * trials
 
+    # exp10. is the same as logspace
+    frex     = exp10.(range(log10(min_freq), log10(max_freq), length=num_frex))
+    s        = exp10.(range(log10(3),log10(10), length=num_frex)) ./ (2 * pi * frex) # scaling factor
 
-# exp10. is the same as logspace
-frex    = exp10.(range(log10(min_freq), log10(max_freq), length=num_frex))
-s       = exp10.(range(log10(3),log10(10), length=num_frex)) ./ (2 * pi * frex) # scaling factor
+    # Initialize
+    datpower = Array{Float64}(undef, num_frex, size(ROI)[2])
+    datphsco = Array{Float64}(undef, num_frex, size(ROI)[2])
 
-# Initialize
-datpower = Array{Float64}(undef, num_frex, size(ROI)[2])
+    baseidx  = ([params.startTime+1 0]) .+ params.startTime*-1 # time of baseline 
 
-baseidx = ([params.startTime+1 0]) .+ params.startTime*-1 # time of baseline 
+    for fi=1:num_frex
 
-for fi=1:num_frex
+        wavelet = sqrt(1/(s[fi]*sqrt(pi))) * exp.(2*1im*pi*frex[fi].*time) .* exp.(-time.^2 ./ (2*(s[fi]^2)))
 
-    wavelet = sqrt(1/(s[fi]*sqrt(pi))) * exp.(2*1im*pi*frex[fi].*time) .* exp.(-time.^2 ./ (2*(s[fi]^2)))
+        datconv = same_conv(ROI[:], wavelet)
+        datconvshaped = reshape(datconv,size(ROI)[2],size(ROI)[3])
+        # Average power over trials (this code performs baseline transform)
+        temppower = mean(abs.(datconvshaped).^2, dims=2) # POWER 
+        temppower = 10*log10.(temppower ./ mean(temppower[baseidx[1]:baseidx[2]]))
+        datpower[fi,:] = temppower
 
-    datconv = same_conv(ROI[:], wavelet)
-    # Average power over trials (this code performs baseline transform)
-    temppower = mean(abs.(reshape(datconv,size(ROI)[2],size(ROI)[3])).^2, dims=2) 
-    # NOTE: this reshaping is currently unnecessary on a single vector of data but I'm leaving it for when I re-examine for multiple trials
-    temppower = 10*log10.(temppower ./ mean(temppower[baseidx[1]:baseidx[2]]))
-    datpower[fi,:] = temppower
+        tempphsco = abs.(mean((datconvshaped ./ abs.(datconvshaped)), dims=2)) # PHASE COHERENCE
+        datphsco[fi,:] = tempphsco
+    end
 
+    power_plot = heatmap(
+        ROItime,
+        frex,
+        datpower,
+        levels=40,
+        clim=(-10,10),
+        xlims=(-200,1170),
+        yaxis=:log,
+        formatter =x->round(Int, x),
+        ytick=exp10.(range(log10(min_freq),log10(max_freq),length=10)),
+        title="Power of " * curAn * " " * curCond * " layer " * params.layers[iLay]
+    );
+
+    
+    phsco_plot = heatmap(
+        ROItime,
+        frex,
+        datphsco,
+        levels=40,
+        clim=(0,1),
+        xlims=(-200,1170),
+        yaxis=:log,
+        formatter =x->round(Int, x),
+        ytick=exp10.(range(log10(min_freq),log10(max_freq),length=10)),
+        title="Phase Co of " * curAn * " " * curCond * " layer " * params.layers[iLay]
+    );
+
+    full_plot = plot(power_plot, phsco_plot, titlefontsize = 10, size=(900,400))
+
+    foldername = "Spectral"
+    if !isdir(joinpath(figs,foldername))
+        mkdir(joinpath(figs,foldername))
+    end
+
+    name = joinpath(figs,foldername,curAn) * "_" * curCond * "_" * params.layers[iLay] * "_ScaloPower.pdf"
+    savefig(full_plot, name)
+
+    return datpower, datphsco
 end
-
-power_plot = heatmap(
-    ROItime,
-    frex,
-    datpower,
-    color=:viridis,
-    levels=40,
-    clim=(-10,10),
-    xlims=(-200,1170),
-    yaxis=:log,
-    formatter =x->round(Int, x),
-    ytick=exp10.(range(log10(min_freq),log10(max_freq),length=6)),
-    title="Logarithmic frequency scaling"
-);
-
-
-plot!(log_freq, titlefontsize = 10, size=(400,400));
-
-foldername = "Spectral"
-if !isdir(joinpath(figs,foldername))
-    mkdir(joinpath(figs,foldername))
-end
-
-name = joinpath(figs,foldername,curAn) * "_" * curCond * "_" * params.layers[iLay] * "_ScaloPower.pdf"
-savefig(power_plot, name)
-
-end
-
 
 function same_conv(signal, kernel)
 
