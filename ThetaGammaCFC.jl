cba9530b7cb11cc595018e7c4a72376d2b761a60
kdeane <katrina.deane@lin-magdeburg.de>
2021-03-12 14:46:39 +0100

    added surrogate test and relevant output to save to a table

--- ThetaGammaCFC.jl
+++ ThetaGammaCFC.jl
@@ -2,7 +2,7 @@
 
 # https://mark-kramer.github.io/Case-Studies-Python/07.html
 
-using MAT, Statistics
+using MAT, Statistics, Random
 using DSP, FFTW
 # python for the filtering:
 using PyCall
@@ -29,7 +29,7 @@ ConIdx = (LinearIndices(anCon))[findall(x -> x == "preCL_1", anCon)] #findall gi
 curCSD = anCSD[ConIdx][1]; # pull out the CSD at that position
 # loop through frequencies 
 # loop through trials
-thistrial = 6
+thistrial = 1
 curCSDst = curCSD[2][:,:,thistrial]  # pull out the 5hz signal for now in the FIRST single trial
 
 # loop through layers (may flip order to layers then trials)
@@ -63,7 +63,7 @@ plot(
     label = ["Signal" "Theta" "Low Gamma" "High Gamma"],
     ylabel = "Amplitude [mV/mm²]",
     xlabel = "Time [ms]",
-    title = "Spectrum",
+    title = "Filters",
 )
 
 phith = angle.(signal.hilbert(Vlo)) # Compute phase of low-freq signal
@@ -78,10 +78,10 @@ phithmean   = zeros(1,length(phasebins)-1) # preallocate
 for iBin = 1:length(phasebins)-1
     phLow = phasebins[iBin]   # lower limit
     phHi  = phasebins[iBin+1] # upper limit
-    ind   = findall(phLow .<= phith .< phHi) # find phases falling in bin
+    ind   = findall(phLow .<= phith .< phHi)   # find phases falling in bin
     amplgmean[iBin] = mean(amplg[ind])         # compute mean amp
     amphgmean[iBin] = mean(amphg[ind])         # compute mean amp
-    phithmean[iBin] = mean([phLow, phHi])    # save center phase 
+    phithmean[iBin] = mean([phLow, phHi])      # save center phase 
 end
 
 ### Determine if the Phase and Amp are related - of a variety of methods, they recommend the phase-amplitude plot:
@@ -103,27 +103,60 @@ hhg = maximum(amphgmean) - minimum(amphgmean)
 ### Assess h's significance by creating a surrogate phase-amp vector by resampling without replacement the amplitude time series (explanation as to why in link above)
 
 nsurrogate = 1000 # number of test surrogates 
-hSlg  = zeros(nsurrogate)
+hSlg  = zeros(nsurrogate) # very like a parametric bootstrap
+hShg  = zeros(nsurrogate) # also very like permuation testing
+
 for iSur = 1:nsurrogate 
 
-    # n_surrogates = 1000;                    #Define no. of surrogates.
-    # hS = zeros(n_surrogates)                #Vector to hold h results.
-    # for ns in range(n_surrogates):          #For each surrogate,
-    #     ampS = amp[randint(0,N,N)]          #Resample amplitude,
-    #     p_bins = arange(-pi, pi, 0.1)       #Define the phase bins
-    #     a_mean = zeros(size(p_bins)-1)      #Vector for average amps.
-    #     p_mean = zeros(size(p_bins)-1)      #Vector for phase bins.
-    #     for k in range(size(p_bins)-1):
-    #         pL = p_bins[k]                  #... lower phase limit,
-    #         pR = p_bins[k+1]                #... upper phase limit.
-    #         indices=(phi>=pL) & (phi<pR)    #Find phases falling in bin,
-    #         a_mean[k] = mean(ampS[indices]) #... compute mean amplitude,
-    #         p_mean[k] = mean([pL, pR])      #... save center phase.
-    #     hS[ns] = max(a_mean)-min(a_mean)    # Store surrogate h.
+    amplgS = shuffle(amplg) # shuffle or resample
+    amphgS = shuffle(amphg)
 
-    # counts, _, _ = hist(hS, label='surrogates')               # Plot the histogram of hS, and save the bin counts.
-    # vlines(h, 0, max(counts), colors='red', label='h', lw=2)  # Plot the observed h,
-    # legend();
+    phasebins = [-pi:0.1:pi...] # To compute CFC, define phase bins,
+    amplgmean   = zeros(1,length(phasebins)-1) # preallocate 
+    amphgmean   = zeros(1,length(phasebins)-1) # preallocate 
+
+    for iBin = 1:length(phasebins)-1
+        phLow = phasebins[iBin]   # lower limit
+        phHi  = phasebins[iBin+1] # upper limit
+        ind   = findall(phLow .<= phith .< phHi)   # find phases falling in bin
+        amplgmean[iBin] = mean(amplgS[ind])        # compute mean amp
+        amphgmean[iBin] = mean(amphgS[ind])        # compute mean amp
+    end
+
+    hSlg[iSur] = maximum(amplgmean) - minimum(amplgmean) # diff between min and max
+    hShg[iSur] = maximum(amphgmean) - minimum(amphgmean)
+    
+end
+
+histogram(
+    [hSlg hShg],
+    label = ["Low gamma surrogate distribution" "High gamma surrogate distribution"],    
+    fillcolor = [:red4 :blue4],
+    title     = "Observed vs Surrogate Distribution",
+    ylabel    = "Count",
+    xlabel    = "Amplitude [mV/mm²]"
+) # the results of the surrogate tests in a distribution
+
+vline!(
+    [hlg hhg],
+    label     = ["Low gamma observed" "High gamma observed"], 
+    linecolor = [:indianred :royalblue2],
+    linewidth = 6
+) # with the observed values overlaid
+
+
+
 ### - To compute a p-value, we determine the proportion of surrogate h values greater than the observed h value
-    # p = sum([s > h for s in hS]) / len(hS)
-    # print(p)
+
+pval_lg = length(findall(hSlg .> hlg)) / length(hSlg) # p value
+pval_hg = length(findall(hShg .> hhg)) / length(hShg)
+
+surmean_lg, surstd_lg = mean(hSlg), std(hSlg) # mean/std of sur dist
+obsdist_lg = hlg - surmean_lg / surstd_lg     # std away from mean 
+
+surmean_hg, surstd_hg = mean(hShg), std(hShg)
+distObs_hg = hhg - surmean_hg / surstd_hg
+
+# need a table with the following: Group Animal Condition Frequency Layer hlg surmean_lg surstd_lg pval_lg obsdist_lg hhg surmean_hg surstd_hg pval_hg obsdist_hg
+
+# then I have a few features to play with but the important thing will be averaging the std distance from the surrogate mean between trials and then animals. This will give a magnitude of significant difference from random and may provide more nuanced insights into differences between groups 
