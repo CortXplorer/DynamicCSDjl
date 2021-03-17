46400e26b06529aeeb811b29e9fb1b949c1615a4
kdeane <katrina.deane@lin-magdeburg.de>
2021-03-17 11:00:24 +0100

    read CSV and compare group averages of STD away from surrogate mean, print out results for initial view

--- ThetaGammaCFC.jl
+++ ThetaGammaCFC.jl
@@ -31,4 +31,76 @@ layers   = ["II" "IV" "V" "VI"]
 # getCFCcsv(home,data,signal,Groups,condList,stimfrq,layers,sr,NQ)
 #---------------------------------------------------------------------------------------
 
-csvTab = CSV.read(joinpath(data,"Spectral\\CFCtable.csv"))
+csvTab = CSV.File(joinpath(data,"Spectral\\CFCtable.csv")) |> DataFrame
+
+# seperate by group
+KIC = csvTab[csvTab[:,:Group] .== "KIC",:]
+KIT = csvTab[csvTab[:,:Group] .== "KIT",:]
+KIV = csvTab[csvTab[:,:Group] .== "KIV",:]
+
+# performing this very roughly now to get a sense for it
+KICpream   = KIC[KIC[:,:Condition] .== "preAM_1",:]
+KICpream   = filter(row -> ! isnan(row.ObsDist_lowgam), KICpream) # find out where nans coming from!!
+CprAM_mean = mean(KICpream.ObsDist_lowgam)
+
+KITpream   = KIT[KIT[:,:Condition] .== "preAM_1",:]
+KITpream   = filter(row -> ! isnan(row.ObsDist_lowgam), KITpream) # find out where nans coming from!!
+TprAM_mean = mean(KITpream.ObsDist_lowgam)
+
+KIVpream   = KIV[KIV[:,:Condition] .== "preAM_1",:]
+KIVpream   = filter(row -> ! isnan(row.ObsDist_lowgam), KIVpream) # find out where nans coming from!!
+VprAM_mean = mean(KIVpream.ObsDist_lowgam)
+
+####
+
+KICam   = KIC[KIC[:,:Condition] .== "AM_1",:]
+KICam   = filter(row -> ! isnan(row.ObsDist_lowgam), KICam) # find out where nans coming from!!
+CAM_mean = mean(KICam.ObsDist_lowgam)
+
+KITam   = KIT[KIT[:,:Condition] .== "AM_1",:]
+KITam   = filter(row -> ! isnan(row.ObsDist_lowgam), KITam) # find out where nans coming from!!
+TAM_mean = mean(KITam.ObsDist_lowgam)
+
+KIVam   = KIV[KIV[:,:Condition] .== "AM_1",:]
+KIVam   = filter(row -> ! isnan(row.ObsDist_lowgam), KIVam) # find out where nans coming from!!
+VAM_mean = mean(KIVam.ObsDist_lowgam)
+
+####
+
+KICprecl   = KIC[KIC[:,:Condition] .== "preCL_1",:]
+KICprecl   = filter(row -> ! isnan(row.ObsDist_lowgam), KICprecl) # find out where nans coming from!!
+CprCL_mean = mean(KICprecl.ObsDist_lowgam)
+
+KITprecl   = KIT[KIT[:,:Condition] .== "preCL_1",:]
+KITprecl   = filter(row -> ! isnan(row.ObsDist_lowgam), KITprecl) # find out where nans coming from!!
+TprCL_mean = mean(KITprecl.ObsDist_lowgam)
+
+KIVprecl   = KIV[KIV[:,:Condition] .== "preCL_1",:]
+KIVprecl   = filter(row -> ! isnan(row.ObsDist_lowgam), KIVprecl) # find out where nans coming from!!
+VprCL_mean = mean(KIVprecl.ObsDist_lowgam)
+
+####
+
+KICcl   = KIC[KIC[:,:Condition] .== "CL_1",:]
+KICcl   = filter(row -> ! isnan(row.ObsDist_lowgam), KICcl) # find out where nans coming from!!
+CCL_mean = mean(KICcl.ObsDist_lowgam)
+
+KITcl   = KIT[KIT[:,:Condition] .== "CL_1",:]
+KITcl   = filter(row -> ! isnan(row.ObsDist_lowgam), KITcl) # find out where nans coming from!!
+TCL_mean = mean(KITcl.ObsDist_lowgam)
+
+KIVcl   = KIV[KIV[:,:Condition] .== "CL_1",:]
+KIVcl   = filter(row -> ! isnan(row.ObsDist_lowgam), KIVcl) # find out where nans coming from!!
+VCL_mean = mean(KIVcl.ObsDist_lowgam)
+
+####
+
+println(["For the pre-laser AM: treated is " TprAM_mean " std away while the naive and viral control are " CprAM_mean " and " VprAM_mean " away respectively."])
+
+println(["For the post-laser AM: treated is " TAM_mean " std away while the naive and viral control are " CAM_mean " and " VAM_mean " away respectively."])
+
+println(["For the pre-laser CL: treated is " TprCL_mean " std away while the naive and viral control are " CprCL_mean " and " VprCL_mean " away respectively."])
+
+println(["For the post-laser CL: treated is " TCL_mean " std away while the naive and viral control are " CCL_mean " and " VCL_mean " away respectively."])
+
+## and there you have it. There doesn't seem to be a difference under anesthesia 
