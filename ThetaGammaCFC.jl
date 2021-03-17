747a7e5897e1dd65d1444f28d760535683d16df0
kdeane <katrina.deane@lin-magdeburg.de>
2021-03-17 09:26:21 +0100

    added progress meter and included trial number in dataframe output

--- ThetaGammaCFC.jl
+++ ThetaGammaCFC.jl
@@ -7,6 +7,7 @@
 using MAT, Statistics, Random
 using DSP, FFTW
 using DataFrames, CSV
+using ProgressMeter
 # python for the filtering:
 using PyCall
 signal = pyimport("scipy.signal")
@@ -27,11 +28,11 @@ sr       = 1000          # sampling rate
 NQ       = Int(sr/2)     # Nyquest frequency
 
 # initialize DataFrame table
-cfcTab = DataFrame(Group = String[], Animal = String[], Condition = String[], StimFrq = String[], Layer = String[], h_lowgam = Float64[], Smean_lowgam = Float64[], Sstd_lowgam = Float64[], p_lowgam = Float64[], ObsDist_lowgam = Float64[], h_higam = Float64[], Smean_higam = Float64[], Sstd_higam = Float64[], p_higam = Float64[], ObsDist_higam = Float64[])
+cfcTab = DataFrame(Group = String[], Animal = String[], Condition = String[], StimFrq = String[], Layer = String[], Trial = Float64[], h_lowgam = Float64[], Smean_lowgam = Float64[], Sstd_lowgam = Float64[], p_lowgam = Float64[], ObsDist_lowgam = Float64[], h_higam = Float64[], Smean_higam = Float64[], Sstd_higam = Float64[], p_higam = Float64[], ObsDist_higam = Float64[])
 
 takepic = 0 # only use this for specific case checking
 
-for iGr = 1:length(Groups) # loop through groups
+@showprogress for iGr = 1:length(Groups) # loop through groups
 
     animalList,_,LIIList,LIVList,LVList,LVIList,_ = callGroup(Groups[iGr]); # extract animal data of group
 
@@ -77,7 +78,7 @@ for iGr = 1:length(Groups) # loop through groups
                         amplg = abs.(signal.hilbert(Vlg))   # Compute amp of low gamma
                         amphg = abs.(signal.hilbert(Vhg))   # Compute amp of high gamma
 
-                        hlg, hhg = CFCget_h(phith,amplg,amphg,1) # h gives the max amp - min amp after creating the curve to couple theta phase to gamma amp
+                        hlg, hhg = CFCget_h(phith,amplg,amphg,takepic) # h gives the max amp - min amp after creating the curve to couple theta phase to gamma amp
 
                         ### Assess h's significance by creating a surrogate phase-amp vector by resampling without replacement the amplitude time series (explanation as to why in link above)
 
@@ -110,7 +111,7 @@ for iGr = 1:length(Groups) # loop through groups
                         surmean_hg, surstd_hg = mean(hShg), std(hShg)
                         distObs_hg = hhg - surmean_hg / surstd_hg
 
-                        push!(cfcTab, [curAn[1:3] curAn condList[iCn] stimfrq[iFr] layers[iLa] hlg surmean_lg surstd_lg pval_lg distObs_lg hhg surmean_hg surstd_hg pval_hg distObs_hg])
+                        push!(cfcTab, [curAn[1:3] curAn condList[iCn] stimfrq[iFr] layers[iLa] iTr hlg surmean_lg surstd_lg pval_lg distObs_lg hhg surmean_hg surstd_hg pval_hg distObs_hg])
 
                         # averaging the std distance of observed from the surrogate mean between trials and then animals will give a magnitude of significant difference from random and may provide more nuanced insights into differences between groups 
                     end # layers
