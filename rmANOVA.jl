fd1c6ed29a44439771afd86bc49bc7860f2ffb12
kdeane <katrina.deane@lin-magdeburg.de>
2021-05-05 11:09:23 +0200

    error if column number doesn't match levels and removal of subject effects

--- rmANOVA.jl
+++ rmANOVA.jl
@@ -4,23 +4,45 @@
 
 home    = @__DIR__
 
-# datX is observation x variable-combination matrix.
+# rawX is observation x variable-combination matrix.
 # levels is vector of levels per factor.
 # variables is a cell array of strings.
 
 ## test data:
 levels    = [2,2] # 2 groups, 2 measurements
 variables = ["Groups" "Measurement"]
-datX      = randn(10,4) # rows: 2 groups of 10 subjects each columns: 2 groups of 2 measurements each - horizontally concatonated
+rawX      = randn(10,4) # rows: 2 groups of 10 subjects each | columns: 2 groups of 2 measurements each - horizontally concatonated
+#function rmAnova(rawX, levels=[2,2], variables=["Var1" "Var2"])
+
+# perform a quick check
+if size(datX,2) != prod(levels,dims=1)[1]
+    error("Ensure the the number of columns equals the product of the levels. E.g.: If there are 2 groups and 2 measurements, there should be 4 columns. 2 goups and 4 measurements should have 8 columns. Horizontally concatonate the groups' vertically stacked measurement results.")
+end
+
+## Get rid of the code below if I'm right that it's not needed:
+# if size(datX,2) > prod(levels,dims=1)[1] # n of observations per cell
+#     b    = size(datX,2) / 2
+#     newX = datX[:,Int(b)+1:end]
+#     datX = datX[:,1:Int(b)] 
+# else
+#     newX = ones(size(datX))
+# end
+
+nSub, nVar = size(rawX,1), size(rawX,2)
+
+# remove and store subject effects
+subEffect = ones(nSub)
+for iSub = 1:nSub
+    fNonNaN = isfinite.(rawX[iSub,:]) # NaN will trigger "false"
+    subEffect[iSub] = sum(rawX[iSub,fNonNaN] .* onesX[iSub,fNonNaN]) ./ sum(onesX[iSub, fNonNaN])
+end
+rawX_sub = subEffect * ones(1,size(rawX,2))
+datX = rawX - rawX_sub
+
+# preallocate ANOVA fields
+# X1, factorStarts, nColsFactor, labels, cellsets = preANOVA(levels, variabls)
+
 
 p_boncor = 0.05 / prod(levels,dims=1)  # bonferroni corrected p value
 
-if size(datX,2) > prod(levels,dims=1)[1] # n of observations per cell
-    b    = size(datX,2) / 2
-    newX = datX[:,Int(b)+1:end]
-    datX = datX[:,1:Int(b)] 
-else
-    newX = ones(size(datX))
-end
 
-nSub, nVar = size(datX,1), size(datX,2)
