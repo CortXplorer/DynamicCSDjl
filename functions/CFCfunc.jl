1ed42a72618d799fe3ae34182b41491c6a55a62b
kdeane <katrina.deane@lin-magdeburg.de>
2021-05-19 14:26:32 +0200

    change variable name to match taking lfp instead of csd

--- functions/CFCfunc.jl
+++ functions/CFCfunc.jl
@@ -1,12 +1,12 @@
-function visSpectrum(layCSDst, sr = 1000)
+function visSpectrum(layLFPst, sr = 1000)
 
-    t   = [1:Int(1000/sr):length(layCSDst)...] #time accounting for sr
+    t   = [1:Int(1000/sr):length(layLFPst)...] #time accounting for sr
     dt  = t[2] - t[1]   # sampling interval
     fNQ = 1 / dt / 2    # Nyquest is half sampling rate (fraction: 0.5)
     N   = length(t)     # number of data points 
-    T   = length(layCSDst) # the total duration of data (T = N here)
+    T   = length(layLFPst) # the total duration of data (T = N here)
 
-    x  = hanning(N) .* layCSDst[:] # multiply data by hanning taper
+    x  = hanning(N) .* layLFPst[:] # multiply data by hanning taper
     xf = fft(x) # compute Fourier transform
     Sxx = (2*dt)^(2/T) * (xf.*conj(xf)) # compute the spectrum
     Sxx = real(Sxx) # ignore complex components
@@ -15,7 +15,7 @@ function visSpectrum(layCSDst, sr = 1000)
     specplot = plot(
         faxis[1:139], 
         10 * log10.(Sxx[1:139]),
-        title  = "Spectrum of CSD FFT",
+        title  = "Spectrum of LFP FFT",
         ylabel = "Power [mV^2/Hz]",
         xlabel = "Frequency [Hz]", 
     )
@@ -45,32 +45,32 @@ function visSurrdist(hSlg, hShg, hlg, hhg)
 
 end
 
-function thetagamma_filter(layCSDst, sr, NQ, signal, takepic=1)
+function thetagamma_filter(layLFPst, sr, NQ, signal, takepic=1)
 
     # set a passband [4-7] Hz 
     Wn  = [4:7...] # theta band
     n   = 100      # filter order (determines accuracy)
     b   = signal.firwin(n, Wn, nyq=NQ, pass_zero=false, window="hamming") #PyCall
-    Vlo = signal.filtfilt(b, 1, layCSDst) #PyCall
+    Vlo = signal.filtfilt(b, 1, layLFPst) #PyCall
 
     # set a passband [31-60] Hz 
     Wn  = [31:60...] #low gamma band (in KIC02 preCL_1 I'm seeing a spike at 50 Hz, I will have to find a way to verify that this is the target of interest accross measurements/animals)
     b   = signal.firwin(n, Wn, nyq=NQ, pass_zero=false, window="hamming")
-    Vlg = signal.filtfilt(b, 1, layCSDst)
+    Vlg = signal.filtfilt(b, 1, layLFPst)
 
     Wn  = [61:100...] #high gamma band (I don't see a spike here but I figured we should still just check it for now)
     b   = signal.firwin(n, Wn, nyq=NQ, pass_zero=false, window="hamming")
-    Vhg = signal.filtfilt(b, 1, layCSDst)
+    Vhg = signal.filtfilt(b, 1, layLFPst)
 
     if takepic == 1 # if we want mass pic production, will need to update naming labels
         ## visualize the spectrum
-        specplot = visSpectrum(layCSDst, sr) # to visualize the SPECTRUM
+        specplot = visSpectrum(layLFPst, sr) # to visualize the SPECTRUM
         savefig("VisSpectrum.png") # yea please make a folder for this output, jesus
         # this is relevant in selecting the filter sizes and will need to be checked again later to verify our choices above
 
         ## visualize the filters
         plot(
-            [layCSDst Vlo Vlg Vhg], 
+            [layLFPst Vlo Vlg Vhg], 
             label = ["Signal" "Theta" "Low Gamma" "High Gamma"],
             ylabel = "Amplitude [mV/mm²]",
             xlabel = "Time [ms]",
