fcadd378939151fb839efe1fdfdc581e1924bfa0
kdeane <katrina.deane@lin-magdeburg.de>
2021-12-02 11:33:23 +0100

    making the animal and group average for all stimuli

--- functions/AvgCSDfig.jl
+++ functions/AvgCSDfig.jl
@@ -15,72 +15,89 @@ end
 
 # plot function -- move to after when making this whole thing a func
 function plot_avgcsd(avgCSDin,figs,Name, Cond)
-    avgcsd = plot(layout = (1,1)) # lul
+    avgcsd = plot(layout = (size(avgCSDin,3))) 
     
-    if ndims(avgCSDin) > 2
-        avgCSDin = avgCSDin[:,:]
+    if ndims(avgCSDin) > 3
+        avgCSDin = avgCSDin[:,:,:]
     end
-    heatmap!(
-        avgcsd, # call the figure that was opened
-        avgCSDin, # plot the map
-        size    = (1480,1000),
-        c       = :jet, # requires Colors.jl
-        clims   = (-0.0003,0.0003),
-        yflip   = true,
-        title   = Name*Cond)
-    
+
+    for icsd = 1:size(avgCSDin,3)
+        heatmap!(
+            avgcsd[icsd], # call the figure that was opened
+            avgCSDin[:,:,icsd], # plot the map
+            size    = (1480,1000),
+            c       = :jet, # requires Colors.jl
+            clims   = (-0.0003,0.0003),
+            yflip   = true,
+            title   = Name*Cond,
+            colorbar= false)
+    end
     name = joinpath(figs,"AvgCSD",Name) * "_" * Cond * "_CSD.pdf"
     savefig(avgcsd, name)
 end
 
-chanlength = 21   # max number of channels
-timeaxis   = 1400 # actual time usually 1377ms
-daysrecord = 7    # days of recording
-subjects   = 4    # number of subjects
-
-GroupList = ["CIC"]
+# best would be to have this information sent into this dynamically but that may be more possible with this whole script turned into a function call
+chanlength  = 21   # max number of channels
+timeaxis    = 1400 # actual time usually 1377ms
+daysrecord  = 7    # days of recording
+subjects    = 4    # number of subjects
+frequencies = 5    # number of stimuli in trials
+# let's say that we would only be working on one group at a time ever
+Group       = "CIC"
 
 # current conditions: "preCL"
 
-for iGr = GroupList # iGr="CIC"
+# preallocate an full group data matrix
+fullGroup = Array{Union{Missing, Float64}}(missing,chanlength,timeaxis,frequencies,daysrecord*subjects) 
 
-    # preallocate an average picture for the group for each condition
-    Group_preCL = Array{Union{Missing, Float64}}(missing,chanlength,timeaxis,daysrecord*subjects)
+# find all files belonging to animals of this group in the data folder
+AnFiles = readdir("Data")[contains.(readdir("Data"),Group)]
+theserows = 0 # indexing variable for placement in data out matrix
 
-    AnFiles = readdir("Data")[contains.(readdir("Data"),iGr)]
-    theserows = 0
-    for iAn = AnFiles # iAn="CIC01_Data.jld2"
-        filename = joinpath(datap,iAn) 
-        Datout = load(filename)[iAn[1:end-5]]
-        curAn = iAn[1:5]
+for iAn = AnFiles # iAn="CIC01_Data.jld2"
 
-        # preallocate an average picture for the animal for each condition
-        CurAn_preCL = Array{Union{Missing, Float64}}(missing,chanlength,timeaxis,daysrecord)
-        for iday = 1:daysrecord # iday=1
+    filename = joinpath(datap,iAn) 
+    Datout = load(filename)[iAn[1:end-5]]
+    curAn = iAn[1:5]
+    
+    # preallocate an average picture for the animal for each condition
+    fullAnimal = Array{Union{Missing, Float64}}(missing,chanlength,timeaxis,frequencies,daysrecord)
+
+    # cycle through the days of recording
+    for iday = 1:daysrecord # iday=1
+        # cycle through the number of stimuli in the trial
+        for ifreq = 1:numfreq # ifreq=1
+
             curDay  = collect(keys(Datout["preCL"]))[iday]
-            thisCSD = Datout["preCL"][curDay][1]["AvgCSD"][2]
-            CurAn_preCL[1:size(thisCSD,1),1:size(thisCSD,2),iday] = thisCSD # the final 2 indicates 5Hz stimulus!
+            thisCSD = Datout["preCL"][curDay][1]["AvgCSD"][ifreq]
+            fullAnimal[1:size(thisCSD,1),1:size(thisCSD,2),ifreq,iday] = thisCSD 
             if iday == daysrecord
-                CurAn_preCL = CurAn_preCL[1:size(thisCSD,1),1:size(thisCSD,2),1:daysrecord] 
+                fullAnimal = fullAnimal[1:size(thisCSD,1),1:size(thisCSD,2),1:frequencies,1:daysrecord] 
             end
-        end # condition
-        avgAn_preCL = mean(CurAn_preCL,dims=3)
-        plot_avgcsd(avgAn_preCL,figs,curAn, "preCL")
 
-        Group_preCL[1:size(CurAn_preCL,1),1:size(CurAn_preCL,2),(1:daysrecord).+theserows] = CurAn_preCL 
-        theserows = theserows + daysrecord 
-    end # animal / entry in data folder
+        end # frequencies
 
-    # cut time length to 1377 ms, the normal amount of time out
-    Group_preCL = Group_preCL[:,1:1377,:]
-    # find the largest size that shoots back non-missing output
-    while ismissing(mean(Group_preCL[:,1,1]))
-        Group_preCL = Group_preCL[1:size(Group_preCL,1)-1,:,:]
-    end
+    end # day of recording
+    
+    # average over days and drop the singleton dimension at the end
+    avgAn_preCL = mean(fullAnimal,dims=4)[:,:,:] 
+    # plot all current stimuli
+    plot_avgcsd(avgAn_preCL,figs,curAn, "preCL")
 
-    avgGr_preCL = mean(Group_preCL,dims=3)
-    plot_avgcsd(avgGr_preCL,figs,iGr,"preCL")
-end # group
+    fullGroup[1:size(fullAnimal,1),1:size(fullAnimal,2),1:numfreq,(1:daysrecord).+theserows] = fullAnimal 
+    theserows = theserows + daysrecord 
+end # animal / entry in data folder
+
+# cut time length to 1377 ms, the normal amount of time out
+fullGroup = fullGroup[:,1:1377,:]
+# find the largest size that shoots back non-missing output
+while ismissing(mean(fullGroup[:,1,1]))
+    fullGroup = fullGroup[1:size(fullGroup,1)-1,:,:]
+end
+
+avgGr_preCL = mean(fullGroup,dims=3)
+plot_avgcsd(avgGr_preCL,figs,iGr,"preCL")
+
 
 
 
